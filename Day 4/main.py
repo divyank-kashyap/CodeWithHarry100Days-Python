ec145071d3ba1be8cbc29b52624a2b3ff6930267
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-02 18:38:06 +0530

    Add files via upload
    Day 4
    Comments, Escape Sequences, end , sep

---
+++ Day 4/main.py
@@ -0,0 +1,15 @@
+# Hello Divyank          "Comment"       
+
+print("My name is Divyank \nI am from Shimla")       # \n is a escape sequence character    
+                                                     # \n is a new line character 
+print("Hello World")        # Printing Hello World
+
+''' Hello Divyank, Please dont remove this line           
+Author : Harry Bhai
+100daysOfCode '''          # multiline comment using '''
+                            # use can also use """"
+
+print("My name is \"Divyank\" I am from Shimla") 
+
+print ("Divyank", 10, 20, sep="_", end="007\n")      # sep is a seperator 
+print("Harry")                                       # end will print in the end 
