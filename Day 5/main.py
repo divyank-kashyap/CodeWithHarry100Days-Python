27c79c258581aca825d3663ba5fd64981b9e0856
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-03 19:31:26 +0530

    Add files via upload
    Data types : tuple, list, dictionary 
    type() function

---
+++ Day 5/main.py
@@ -0,0 +1,24 @@
+a = 1
+b = True
+c = "Divyank"
+d = None
+e = complex(8, 2)
+print(e)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+print("The type of e is ", type(e))
+
+list1 = [8, 10.2, [10, 20], "Divyank" , "Akshay"]
+print(list1)
+print("Data type of list1 is ", type(list1))
+
+tuple1 = (("parrot", "eagle"), "Tiger", "Wolf")
+print(tuple1)
+print("Data type of tuple1 is ", type(tuple1))
+
+
+dict1 = {"Name ": "Divyank", "Age" : 18, "CanVote" : False}
+print(dict1)
+print("Data type of dict1 is ", type(dict1))
