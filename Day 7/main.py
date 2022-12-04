ea6414a1abb9352bec6918e9493df7611508163e
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-04 18:09:26 +0530

    Add files via upload
    Calculator (No use of if-else)

---
+++ Day 7/main.py
@@ -0,0 +1,10 @@
+a = 20
+b = 10
+print("Addition ")
+print(a ,"+",b , "=",(a+b), "\n")
+print("Subtraction ")
+print(a , "-",b , "=",(a-b), "\n")
+print("Multiplication ")
+print(a , "*",b , "=",(a*b), "\n")
+print("Division ")
+print(a , "/",b , "=",(a/b)), "\n"
