1367e2ac4cbd9163cc39d6df7c1ae2e824eee9bc
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-05 17:29:09 +0530

    Add files via upload
    Just Revision (Calculator) ;/

---
+++ Day 8/main.py
@@ -0,0 +1,6 @@
+a = 50 
+b = 3
+print("The value of a ", a, "+", b, "is", a+b)
+print("The value of a ", a, "-", b, "is", a-b)
+print("The value of a ", a, "*", b, "is", a*b)
+print("The value of a ", a, "/", b, "is", a/b)
