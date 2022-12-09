9c4d7b4c6237cf9d0e8322c926359b69149fd072
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-09 20:04:38 +0530

    Add files via upload
    String Slicing

---
+++ Day 12/main.py
@@ -0,0 +1,23 @@
+names = "Harry,Shubham"
+print(len(names))
+print(names[0:5])
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:4])
+print(fruit[:5])
+print(fruit[0:-3])
+print(fruit[0:len(fruit)-3])
+print(fruit[-3:-1])
+
+# Quick Quiz
+nm = "Harry"
+print(nm[-4:-2])
+
+
+
+
+
+
