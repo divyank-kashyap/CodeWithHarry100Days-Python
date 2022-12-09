cbde5c2c88bd3edd2ccb39d604914061360bd20f
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-09 15:48:58 +0530

    Add files via upload
    Strings in python

---
+++ Day 11/main.py
@@ -0,0 +1,18 @@
+name = "Harry"
+friend = "Rohan"
+anotherfriend = 'Akshay'
+
+apple = """He wants to "eat" an apple
+hi harry
+hey i am good"""
+# print(apple)
+print("hello, ", name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5])  # Throws an error
+print("Using a for loop\n")
+for character in apple:
+  print(character)
