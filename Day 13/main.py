c156fecdec347c8387b9c57aaab007cf84e6f5ca
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-10 19:36:00 +0530

    Add files via upload
    String Methods in Python

---
+++ Day 13/main.py
@@ -0,0 +1,35 @@
+#  Strings are immmutable
+a = "!!!Harry!!!!! !!!!!!Harry Harry"
+print(len(a))
+print()
+print(a.upper())
+print(a.lower())
+print(a.strip("!"))
+print(a.replace("Harry", "Jhon"))
+print(a.split(" "))
+blockHeading = "introduction to Js"
+print(blockHeading.capitalize())
+
+str1 = "Welcome to console!!!"
+print(str1.center(50))
+print(len(str1))
+print(len(str1.center(50)))
+
+
+str1 = "Welcome to console!!"
+print(str1.endswith("!!"))
+
+str1 = "He's name is Den. He is an honest man."
+print(str1.find("is"))
+print(str1.find("iSHH"))
+# print(str1.index("iSHH"))
+
+str1 = "Welcome to console!!!"
+print(str1.isalnum())
+str1 = "Welcome"
+print(str1.isalpha())
+
+
+ 
+
+
